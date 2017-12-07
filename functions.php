c02b6691b3c031950c934f372b66bf6687978e85
fabolivark <fabian.altahona@koombea.com>
2017-12-07 13:50:50 -0500

    Single page and page styles

--- functions.php
+++ functions.php
@@ -23,6 +23,8 @@ if( ! class_exists( 'Helios' ) ) :
 		public function theme_context( $context = null ) {
 			global $kili_framework;
 
+
+
 			$context['html5shiv'] = $kili_framework->asset_path( 'scripts/html5shiv.js' );
 			$context['respond'] = $kili_framework->asset_path( 'scripts/respond.min.js' );
 			$context['ie8css'] = $kili_framework->asset_path( 'styles/ie8.css' );
@@ -36,13 +38,35 @@ if( ! class_exists( 'Helios' ) ) :
 				'primary_navigation' => __( 'Primary Navigation', 'helios' ),
 				'footer_navigation' => __( 'Footer Navigation', 'helios' ),
 			) );
+
+			//Add theme Support
+			add_theme_support( 'post-thumbnails' );
+
 			$kili_framework->render_pages( $this->theme_context() );
 		}
+
 		public function add_actions() {
+			// Init Widgets
+			add_action( 'widgets_init', array($this, 'helios_widgets_init') );
+
 			if (!is_admin()) {
 				add_action( 'wp_enqueue_scripts', array($this, 'load_assets') );
 			}
 		}
+
+		public function helios_widgets_init() {
+			register_sidebar( array(
+				'name' => __( 'Main Sidebar', 'helios' ),
+				'id' => 'sidebar-1',
+				'description' => __( 'Appears on posts and pages except the optional Front Page template, which has its own widgets', 'helios' ),
+				'before_widget' => '<section id="%1$s" class="%2$s">',
+				'after_widget' => '</section><hr>',
+				'before_title' => '<h3>',
+				'after_title' => '</h3>',
+				)
+			);
+		}
+
 		public function load_assets() {
 			global $kili_framework;
 			wp_enqueue_style( 'theme-style', $kili_framework->asset_path( 'styles/main.css' ), false, null );
---
+++ page-templates/layout-builder.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Template Name: Layout Builder
+ * Template Post Type: page
+ * Description: The template for displaying the Kili Blocks
+ */
